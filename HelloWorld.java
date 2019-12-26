bca8d28cf482e04e070e0c9e856d7a0b53d8740c
Rahul <rraj29916@gmail.com>
2019-12-27 02:12:36 +0530

    second Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
-package Nivea;
-public class HelloWorld
+package Nivea;       //its is my package name which take java.class file 
+public class HelloWorld         //helloWorld is a class name
 {
-	public static void main(String[] args)
+	public static void main(String[] args)            
 	{
-		System.out.println("Hello Java");
+		System.out.println("Hello Java");            //print statement
 	}
 }
